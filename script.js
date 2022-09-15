fc0c47701ab562f08d79d6e5a7eb40fd108ac10a
kishorekumer78 <kishore9657>
2022-09-16 02:13:57 +0600

    added basic ui and changed functionality of game as per ui

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorBtn = document.querySelector(".scissor");
+const resetBtn = document.querySelector(".resetBtn");
+
+const userRoundResultDisplayer = document.querySelector(".userInput");
+const compRoundResultDisplayer = document.querySelector(".compInput");
+const roundResultDisplayer = document.querySelector(".roundResult");
+
+const playerScore = document.querySelector(".playerScore");
+const compScore = document.querySelector(".compScore");
+const drawScore = document.querySelector(".drawScore");
+let userWin = 0;
+let compWin = 0;
+let draw = 0;
+playerScore.textContent = userWin;
+compScore.textContent = compWin;
+drawScore.textContent = draw;
 let choices = ["rock", "paper", "scissors"];
 const getComputerChoice = () => {
   let randNum = Math.floor(Math.random() * 3);
@@ -18,33 +36,89 @@ const getResult = (userChoice, compChoice) => {
   }
 };
 
-const startGame = () => {
-  let userWin = 0;
-  let compWin = 0;
-  let draw = 0;
-  for (let i = 1; i <= 5; i++) {
-    let userInput = prompt("Choose from rock/paper/scissors");
-    console.log(`User no ${i}:  ${userInput}`);
-    let compInput = getComputerChoice();
-    console.log(`Computer no ${i}:  ${compInput}`);
-    if (getResult(userInput, compInput) === "user") {
-      userWin = userWin + 1;
-      console.log("User Wins");
-    } else if (getResult(userInput, compInput) === "computer") {
-      compWin = compWin + 1;
-      console.log("Computer Wins");
-    } else if (getResult(userInput, compInput) === "draw") {
-      draw = draw + 1;
-      console.log("Draw");
-    }
+const countScore = (result) => {
+  if (result === "user") {
+    userWin = userWin + 1;
+    // update score in ui
+    playerScore.textContent = userWin;
+  } else if (result === "computer") {
+    compWin = compWin + 1;
+    // update ui
+    compScore.textContent = compWin;
+  } else if (result === "draw") {
+    draw = draw + 1;
+    // update ui
+    drawScore.textContent = draw;
   }
-  if (userWin > compWin) {
-    console.log("Winner : User");
-  } else if (compWin > userWin) {
-    console.log("Winner : Computer");
-  } else {
-    console.log("Match Draw!");
+};
+const playRound = (userInput) => {
+  // get computer choice
+  let compInput = getComputerChoice();
+  compRoundResultDisplayer.textContent = compInput.toUpperCase();
+
+  if (getResult(userInput, compInput) === "user") {
+    // action for user win
+    roundResultDisplayer.textContent = "YOU WIN!";
+    // add 1 to playerWin counter
+    countScore("user");
+  } else if (getResult(userInput, compInput) === "computer") {
+    // action for computer win
+    countScore("computer");
+    roundResultDisplayer.textContent = "COMPUTER WINS!";
+    // add 1 to compWin counter
+  } else if (getResult(userInput, compInput) === "draw") {
+    // action for draw
+    countScore("draw");
+    roundResultDisplayer.textContent = "DRAW FOR THIS ROUND!";
+  }
+
+  // final result checker
+  if (userWin + compWin + draw === 5) {
+    if (userWin > compWin) {
+      showFinalResult("user");
+    } else if (compWin > userWin) {
+      showFinalResult("computer");
+    } else {
+      showFinalResult("draw");
+    }
   }
 };
 
-startGame();
+const showFinalResult = (winner) => {
+  let text = "Result of Game";
+  if (winner === "user") {
+    text = "Congratulations! You win";
+  } else if (winner === "computer") {
+    text = "Computer won! Better luck next time";
+  } else if (winner === "draw") {
+    text = "Game is draw!";
+  }
+  playerScore.remove();
+  compScore.remove();
+  drawScore.remove();
+  const finalResult = document.createElement("h3");
+  finalResult.innerText = text;
+  const scoreContainer = document.querySelector(".scoreContainer");
+  scoreContainer.textContent = "";
+  scoreContainer.appendChild(finalResult);
+  resetBtn.style.display = "inline";
+};
+
+// startGame();
+
+rockBtn.addEventListener("click", () => {
+  userRoundResultDisplayer.textContent = "ROCK";
+  playRound("rock");
+});
+paperBtn.addEventListener("click", () => {
+  userRoundResultDisplayer.textContent = "PAPER";
+  playRound("paper");
+});
+scissorBtn.addEventListener("click", () => {
+  userRoundResultDisplayer.textContent = "SCISSOR";
+  playRound("scissors");
+});
+
+resetBtn.addEventListener("click", () => {
+  location.reload();
+});
